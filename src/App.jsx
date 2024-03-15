918ef687fdb4bd9196acee40b09985716872fe24
Melvin <melsava1207@gmail.com>
2024-03-15 16:38:18 -0600

    paginas y routeo de paginas creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import MainPage from "./pages/MainPage"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
 
 function App() {
   return (
     <>
-      <h1>Hola soy Melvin</h1>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route  path="/" element={<MainPage/>}/>
+            <Route  path="/login" element={<Login/>}/>
+            <Route  path="/register" element={<Register/>}/>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer/>
     </>
   )
 }
